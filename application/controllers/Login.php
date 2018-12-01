1147542e3ee74aaa4707553db56fb7202c2d5d07
Ennio Gonzalez <mr.ennio@gmail.com>
2018-12-01 19:26:41 -0400

    modificaciones https en el servidor

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,6 +1,8 @@
 
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, OPTIONS");
 
 class Login extends CI_Controller{
 
