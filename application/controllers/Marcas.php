8d17a497f8b999c506e1099f5906a1093b1418ed
Ennio Gonzalez <mr.ennio@gmail.com>
2018-12-24 14:23:40 -0400

    Agregar Auditorias a marcas

--- application/controllers/Marcas.php
+++ application/controllers/Marcas.php
@@ -7,6 +7,51 @@
             $this->load->model('marcas_model');
             $this->load->library('liblistasdesplegables','liblistasdesplegables');
         }
+        
+        private function FormatearBusqueda($datos){
+            
+            $data = array(
+                "Listas"     =>"",
+                "Registros" => ""
+            );
+
+            if($datos){
+                $htmlListas = "";
+                $registros = 0;
+                foreach ($datos as $elemento) {
+                    $registros = $elemento['registros'];
+                    $htmlListas = $htmlListas
+                        ."<tr>"
+                        .   "<td style='display:none;'>" . $elemento['mar_id'] . "</td>"
+                        .   "<td>" . $elemento['nombre'] . "</td>"
+                        .   "<td style='display:none;'>" . $elemento['observaciones'] . "</td>"
+                        ."</tr>";
+                }
+                
+                $data['Listas'] = $htmlListas;
+                $data['Registros'] = $registros;
+            }
+
+            return $data;
+        }
+
+        private function FormatearImpresion($respuesta){
+
+            $data = array(
+                "mar_id"        =>"",
+                "nombre"        =>"",
+                "ubicacion"     =>"",
+                "tipo"          =>"",
+                "cap_amp"       =>"",
+                "nombrepadre"   =>"",
+                "observaciones" => ""
+            );
+
+            if($respuesta)
+                $data = $respuesta;
+
+            return $data;
+        }
 
         private function FormatearRequest($respuesta){
 
@@ -25,6 +70,88 @@
             if(!$this->session->userdata("Permisos")['Marcas']){
                 show_404();
             }
+        }
+
+        public function busqueda(){
+
+            if(!$this->session->userdata("nombre") || $this->input->post("Pagina") == ""){
+                redirect(site_url(''));
+            }
+
+            $busqueda = $this->input->post("Busqueda") ;
+            $pagina = (int) $this->input->post("Pagina") ;
+            $regXpag = (int) $this->input->post("RegistrosPorPagina") ;
+            $ordenamiento = $this->input->post("Orden") ;
+            
+            $inicio = 1+$regXpag*($pagina-1);
+            $fin = $regXpag*$pagina;
+
+            $respuesta = $this->FormatearBusqueda($this->marcas_model->Busqueda($busqueda,$ordenamiento,$inicio,$fin));
+
+            echo json_encode(array("isValid"=>true,"Datos"=>$respuesta));
+        }
+
+        public function eliminar(){
+            
+            $this->ValidarPermiso();
+            if(!$this->session->userdata("nombre") || $this->input->post("id") == ""){
+                redirect(site_url(''));
+            }
+
+            $eliminar = $this->marcas_model->Eliminar($this->input->post("id"));
+            $data = $this->FormatearRequest($this->marcas_model->Obtener());
+            echo json_encode(array("isValid"=>true,"Datos"=>$data));
+        }
+
+        public function guardar(){
+
+            $this->ValidarPermiso();
+
+            if(!$this->session->userdata("nombre") || $this->input->post("Nombre") == ""){
+                redirect(site_url(''));
+            }
+
+            $parametros = array(
+                "mar_id"      => $this->input->post("id"),
+                "Nombre"        => $this->input->post("Nombre"),
+                "Observaciones" => trim($this->input->post("Observacion"))
+            );
+            
+            if($this->input->post("id") == ""){
+                if($this->marcas_model->ExisteNombre($parametros['Nombre'])){
+                    echo json_encode(array(
+                        "isValid"=>false,
+                        "Mensaje"=>"Ya existe una marca registrada con el mismo nombre.",
+                        "id"=>""));
+                }else{
+                    $respuesta = $this->marcas_model->Insertar($parametros);
+                    $insertado = $this->marcas_model->Obtener();
+                    echo json_encode(array(
+                        "isValid"=>true,
+                        "Mensaje"=>"Se ha insertado marca exitosamente",
+                        "id"=>$insertado['mar_id']));
+                }
+            }else{
+                if($this->marcas_model->ExisteNombre($parametros['Nombre'],$parametros['mar_id'])){
+                    echo json_encode(array(
+                        "isValid"=>false,
+                        "Mensaje"=>"Ya existe una marca registrada con el mismo nombre.",
+                        "id"=>""));
+                }else{
+                    $respuesta = $this->marcas_model->Actualizar($parametros);
+                    echo json_encode(array(
+                        "isValid"=>true,
+                        "Mensaje"=>"Se ha editado marca exitosamente",
+                        "id"=>$this->input->post("id")));
+                }
+
+            }
+        }
+
+        public function imprimir($id){
+            $data['datos'] = $this->FormatearImpresion($this->marcas_model->ObtenerInfoPDF($id));
+            $this->load->library('tcpdf/Pdf');
+            $this->load->view('Formatos/formatoMarca',$data);
         }
         
         public function view(){
@@ -59,133 +186,6 @@
             $this->load->view('plantillas/7-footer');
 
 
-        }
-
-        public function guardar(){
-
-            $this->ValidarPermiso();
-
-            if(!$this->session->userdata("nombre") || $this->input->post("Nombre") == ""){
-                redirect(site_url(''));
-            }
-
-            $parametros = array(
-                "idActual"      => $this->input->post("id"),
-                "Nombre"        => $this->input->post("Nombre"),
-                "Observaciones" => trim($this->input->post("Observacion"))
-            );
-            
-            if($this->input->post("id") == ""){
-                if($this->marcas_model->ExisteNombre($parametros['Nombre'])){
-                    echo json_encode(array(
-                        "isValid"=>false,
-                        "Mensaje"=>"Ya existe una marca registrada con el mismo nombre.",
-                        "id"=>""));
-                }else{
-                    $respuesta = $this->marcas_model->Insertar($parametros);
-                    $insertado = $this->marcas_model->Obtener();
-                    echo json_encode(array(
-                        "isValid"=>true,
-                        "Mensaje"=>"Se ha insertado marca exitosamente",
-                        "id"=>$insertado['mar_id']));
-                }
-            }else{
-                if($this->marcas_model->ExisteNombre($parametros['Nombre'],$parametros['idActual'])){
-                    echo json_encode(array(
-                        "isValid"=>false,
-                        "Mensaje"=>"Ya existe una marca registrada con el mismo nombre.",
-                        "id"=>""));
-                }else{
-                    $respuesta = $this->marcas_model->Actualizar($parametros);
-                    echo json_encode(array(
-                        "isValid"=>true,
-                        "Mensaje"=>"Se ha editado marca exitosamente",
-                        "id"=>$this->input->post("id")));
-                }
-
-            }
-        }
-
-        public function eliminar(){
-            
-            $this->ValidarPermiso();
-            if(!$this->session->userdata("nombre") || $this->input->post("id") == ""){
-                redirect(site_url(''));
-            }
-
-            $eliminar = $this->marcas_model->Eliminar($this->input->post("id"));
-            $data = $this->FormatearRequest($this->marcas_model->Obtener());
-            echo json_encode(array("isValid"=>true,"Datos"=>$data));
-        }
-
-        public function busqueda(){
-
-            if(!$this->session->userdata("nombre") || $this->input->post("Pagina") == ""){
-                redirect(site_url(''));
-            }
-
-            $busqueda = $this->input->post("Busqueda") ;
-            $pagina = (int) $this->input->post("Pagina") ;
-            $regXpag = (int) $this->input->post("RegistrosPorPagina") ;
-            $ordenamiento = $this->input->post("Orden") ;
-            
-            $inicio = 1+$regXpag*($pagina-1);
-            $fin = $regXpag*$pagina;
-
-            $respuesta = $this->FormatearBusqueda($this->marcas_model->Busqueda($busqueda,$ordenamiento,$inicio,$fin));
-
-            echo json_encode(array("isValid"=>true,"Datos"=>$respuesta));
-        }
-
-        public function imprimir($id){
-            $data['datos'] = $this->FormatearImpresion($this->marcas_model->ObtenerInfoPDF($id));
-            $this->load->library('tcpdf/Pdf');
-            $this->load->view('Formatos/formatoMarca',$data);
-        }
-
-        private function FormatearImpresion($respuesta){
-
-            $data = array(
-                "mar_id"        =>"",
-                "nombre"        =>"",
-                "ubicacion"     =>"",
-                "tipo"          =>"",
-                "cap_amp"       =>"",
-                "nombrepadre"   =>"",
-                "observaciones" => ""
-            );
-
-            if($respuesta)
-                $data = $respuesta;
-
-            return $data;
-        }
-        
-        private function FormatearBusqueda($datos){
-            
-            $data = array(
-                "Listas"     =>"",
-                "Registros" => ""
-            );
-
-            if($datos){
-                $htmlListas = "";
-                $registros = 0;
-                foreach ($datos as $elemento) {
-                    $registros = $elemento['registros'];
-                    $htmlListas = $htmlListas
-                        ."<tr>"
-                        .   "<td style='display:none;'>" . $elemento['mar_id'] . "</td>"
-                        .   "<td>" . $elemento['nombre'] . "</td>"
-                        .   "<td style='display:none;'>" . $elemento['observaciones'] . "</td>"
-                        ."</tr>";
-                }
-                
-                $data['Listas'] = $htmlListas;
-                $data['Registros'] = $registros;
-            }
-
-            return $data;
         }
 
     }
--- application/models/Marcas_model.php
+++ application/models/Marcas_model.php
@@ -2,64 +2,221 @@
 <?php
     class Marcas_model extends CI_Model{
         
-        public function Insertar($data){
-            //Abrir conexion
-            $conexion = $this->bd_model->ObtenerConexion();
-
-            //Insertar Marcas
-            $query = " INSERT INTO Marcas(Nombre,Observaciones) VALUES('"
-            . str_replace("'", "''",$data['Nombre']) . "',"
-            . (($data['Observaciones'] == "") ? "null" : ("'" .str_replace("'", "''", $data['Observaciones']) . "'"))
-            . ");";
-
-            //Ejecutar Query
-            $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
-            
-            //Liberar memoria
-            pg_free_result($result);
-
-            //liberar conexion
-            $this->bd_model->CerrarConexion($conexion);
-            
-            //Si existe registro, se guarda. Sino se guarda false
-            if ($result)
-                $retorno = true;
-            else
-                $retorno = false;
-
-
-            return $retorno;
-        }
-
         public function Actualizar($data){
             
             //Abrir conexion
             $conexion = $this->bd_model->ObtenerConexion();
 
+            //Abrir Transaccion
+            pg_query("BEGIN") or die("Could not start transaction");
+
             $query = " UPDATE Marcas "
                 . " SET Nombre ='". str_replace("'", "''",$data['Nombre']) 
                 . "', Observaciones = " 
                 .(($data['Observaciones'] == "") ? "null" : ("'" .str_replace("'", "''", $data['Observaciones']) . "'"))
-                . " WHERE MAR_ID = '" . str_replace("'", "''",$data['idActual']) . "';";
+                . " WHERE MAR_ID = '" . str_replace("'", "''",$data['mar_id']) . "';";
 
+
+            //Ejecutar Query
+            $result = pg_query($query);
+            
+            if($result){
+                $datos = array(
+                    'Opcion' => 'Actualizar',
+                    'Tabla' => 'Marcas', 
+                    'Tab_id' => $data['mar_id'],
+                    'Datos' => json_encode($data)
+                );
+                
+                $result = $this->auditorias_model->Insertar($datos);
+            }
+            
+            if(!$result){
+                $error = pg_last_error();
+                pg_query("ROLLBACK") or die("Transaction rollback failed");
+                die($error);
+            }else
+                pg_query("COMMIT") or die("Transaction commit failed");
+
+            //liberar conexion
+            $this->bd_model->CerrarConexion($conexion);
+
+            return true;
+        }
+
+        public function Busqueda($busqueda,$orden,$inicio,$fin){
+            
+            //Abrir conexion
+            $conexion = $this->bd_model->ObtenerConexion();
+            $condicion ="";
+
+
+            if($busqueda != ""){
+                $condicion = " WHERE (LOWER(Nombre) like '%" . strtolower(str_replace(" ","%",str_replace("'", "''",$busqueda)))
+                            . "%')";
+            }
+
+            //Query para buscar usuario
+            $query ="   SELECT  MAR_ID,
+                                Nombre,
+                                Observaciones,
+                                Registros
+                        FROM (
+                            SELECT  MAR_ID,
+                                    Nombre,
+                                    COALESCE(Observaciones,'') Observaciones,
+                                    COUNT(*) OVER() AS Registros,
+                                    ROW_NUMBER() OVER(ORDER BY " . $orden .") Fila
+                            FROM Marcas
+                            " . $condicion . "
+
+                        ) LD
+                        WHERE Fila BETWEEN ". $inicio . " AND " . $fin . "
+                        ORDER BY Fila ASC;";
 
             //Ejecutar Query
             $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
             
+            //Si existe registro, se guarda. Sino se guarda false
+            if ($result){
+                $retorno = [];
+                while($line = pg_fetch_array($result, null, PGSQL_ASSOC))
+                    array_push($retorno,$line);
+
+            } else
+                $retorno = false;
+
             //Liberar memoria
             pg_free_result($result);
 
             //liberar conexion
             $this->bd_model->CerrarConexion($conexion);
 
+            return $retorno;
+        }
+        
+        public function Eliminar($id){
+                  
+            $datosActual = $this->Obtener($id);
+            
+            //Abrir conexion
+            $conexion = $this->bd_model->ObtenerConexion();
+
+            //Abrir Transaccion
+            pg_query("BEGIN") or die("Could not start transaction");
+    
+            //Eliminar Marcas
+            $query = " DELETE FROM Marcas "
+                . " WHERE MAR_ID = '" .str_replace("'", "''",$id) . "';";
+                
+            //Ejecutar Query
+            $result = pg_query($query);
+            
+            if($result){
+                $datos = array(
+                    'Opcion' => 'Eliminar',
+                    'Tabla' => 'Marcas', 
+                    'Tab_id' => $id,
+                    'Datos' => json_encode($datosActual)
+                );
+                
+                $result = $this->auditorias_model->Insertar($datos);
+            }
+
+            if(!$result){
+                $error = pg_last_error();
+                pg_query("ROLLBACK") or die("Transaction rollback failed");
+                die($error);
+            }else
+                pg_query("COMMIT") or die("Transaction commit failed");
+
+            //liberar conexion
+            $this->bd_model->CerrarConexion($conexion);
+
+            return true;
+        }
+
+        public function ExisteNombre($Nombre,$id=""){
+
+            //Abrir conexion
+            $conexion = $this->bd_model->ObtenerConexion();
+    
+            //Query para buscar usuario
+            $query =" SELECT * FROM Marcas WHERE LOWER(Nombre) ='" . strtolower(str_replace("'", "''",$Nombre)) . "' " ;
+
+            if($id != "")
+                $query = $query . " AND MAR_ID <>'" . str_replace("'", "''",$id) . "' " ;
+
+                
+            $query = $query . ";" ;
+
+            
+
+            //Ejecutar Query
+            $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
+            
             //Si existe registro, se guarda. Sino se guarda false
-            if ($result)
+            if (pg_num_rows($result) > 0) 
                 $retorno = true;
             else
                 $retorno = false;
 
+            //Liberar memoria
+            pg_free_result($result);
+
+            //liberar conexion
+            $this->bd_model->CerrarConexion($conexion);
 
             return $retorno;
+        }
+
+        public function Insertar($data){
+
+            //Abrir conexion
+            $conexion = $this->bd_model->ObtenerConexion();
+
+            //Abrir Transaccion
+            pg_query("BEGIN") or die("Could not start transaction");
+
+            //Insertar Marcas
+            $query = " INSERT INTO Marcas(Nombre,Observaciones) VALUES('"
+            . str_replace("'", "''",$data['Nombre']) . "',"
+            . (($data['Observaciones'] == "") ? "null" : ("'" .str_replace("'", "''", $data['Observaciones']) . "'"))
+            . ") RETURNING mar_id;";
+
+            //Ejecutar Query
+            $result = pg_query($query);
+            
+            $new_id = "";
+            if($result){
+                $row = pg_fetch_row($result); 
+                $new_id = $row['0']; 
+            }
+            
+            if($result){
+                $data['mar_id'] = $new_id;
+                $datos = array(
+                    'Opcion' => 'Insertar',
+                    'Tabla' => 'Marcas', 
+                    'Tab_id' => $data['mar_id'],
+                    'Datos' => json_encode($data)
+                );
+                
+                $result = $this->auditorias_model->Insertar($datos);
+            }
+
+            if(!$result){
+                $error = pg_last_error();
+                pg_query("ROLLBACK") or die("Transaction rollback failed");
+                die($error);
+            }else
+                pg_query("COMMIT") or die("Transaction commit failed");
+
+
+            //liberar conexion
+            $this->bd_model->CerrarConexion($conexion);
+            
+            return true;
         }
 
         public function Obtener($id = ''){
@@ -118,120 +275,6 @@
             //Si existe registro, se guarda. Sino se guarda false
             if ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) 
                 $retorno = $line;
-            else
-                $retorno = false;
-
-            //Liberar memoria
-            pg_free_result($result);
-
-            //liberar conexion
-            $this->bd_model->CerrarConexion($conexion);
-
-            return $retorno;
-        }
-
-        public function Busqueda($busqueda,$orden,$inicio,$fin){
-            
-            //Abrir conexion
-            $conexion = $this->bd_model->ObtenerConexion();
-            $condicion ="";
-
-
-            if($busqueda != ""){
-                $condicion = " WHERE (LOWER(Nombre) like '%" . strtolower(str_replace(" ","%",str_replace("'", "''",$busqueda)))
-                            . "%')";
-            }
-
-            //Query para buscar usuario
-            $query ="   SELECT  MAR_ID,
-                                Nombre,
-                                Observaciones,
-                                Registros
-                        FROM (
-                            SELECT  MAR_ID,
-                                    Nombre,
-                                    COALESCE(Observaciones,'') Observaciones,
-                                    COUNT(*) OVER() AS Registros,
-                                    ROW_NUMBER() OVER(ORDER BY " . $orden .") Fila
-                            FROM Marcas
-                            " . $condicion . "
-
-                        ) LD
-                        WHERE Fila BETWEEN ". $inicio . " AND " . $fin . "
-                        ORDER BY Fila ASC;";
-
-            //Ejecutar Query
-            $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
-            
-            //Si existe registro, se guarda. Sino se guarda false
-            if ($result){
-                $retorno = [];
-                while($line = pg_fetch_array($result, null, PGSQL_ASSOC))
-                    array_push($retorno,$line);
-
-            } else
-                $retorno = false;
-
-            //Liberar memoria
-            pg_free_result($result);
-
-            //liberar conexion
-            $this->bd_model->CerrarConexion($conexion);
-
-            return $retorno;
-        }
-        
-        public function Eliminar($id){
-
-            
-            //Abrir conexion
-            $conexion = $this->bd_model->ObtenerConexion();
-    
-            //Eliminar Marcas
-            $query = " DELETE FROM Marcas "
-                . " WHERE MAR_ID = '" .str_replace("'", "''",$id) . "';";
-                
-            //Ejecutar Query
-            $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
-            
-            //Si existe registro, se guarda. Sino se guarda false
-            if ($result) 
-                $retorno = true;
-            else
-                $retorno = false;
-
-            //Liberar memoria
-            pg_free_result($result);
-
-            //liberar conexion
-            $this->bd_model->CerrarConexion($conexion);
-
-            return $retorno;
-
-        }
-
-        public function ExisteNombre($Nombre,$id=""){
-
-            //Abrir conexion
-            $conexion = $this->bd_model->ObtenerConexion();
-    
-            //Query para buscar usuario
-            $query =" SELECT * FROM Marcas WHERE LOWER(Nombre) ='" . strtolower(str_replace("'", "''",$Nombre)) . "' " ;
-
-            if($id != "")
-                $query = $query . " AND MAR_ID <>'" . str_replace("'", "''",$id) . "' " ;
-
-                
-            $query = $query . ";" ;
-
-            
-
-            //Ejecutar Query
-            $result = pg_query($query) or die('La consulta fallo: ' . pg_last_error());
-            
-            //Si existe registro, se guarda. Sino se guarda false
-            if (pg_num_rows($result) > 0) 
-                $retorno = true;
             else
                 $retorno = false;
 
